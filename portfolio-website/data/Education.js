b59a78b2a27fc880c203ec0fae9e97485bd1786f
Nate96 <braukhoff96@gmail.com>
2022-11-04 17:15:32 -0500

    added data

---
+++ portfolio-website/data/Education.js
@@ -0,0 +1,7 @@
+export default [
+    {
+        organizatoin: 'University of Wisconsin - Platteville',
+        locaton: 'Platteville, WI',
+        name: 'Bachelor of Science - Software Engineering'
+    }
+]
---
+++ portfolio-website/data/Research.js
@@ -0,0 +1,15 @@
+export default [
+    {
+        organization: 'University of Wisconsin-Platteville',
+        location: 'Platteville, WI',
+        duraction: '5/2019 - 8/2019',
+        mission_statment: 'Our research aims to create a tool for both students and faculty advisors to predict and prevent academic struggle',
+        highlights: [
+            'Built a Bayesian Network from scratch using python',
+            'Constructed the university curriculum into a Binary Tree',
+            'Solved architecture problems using OO methods when designing our Bayesian Network',
+            'Use over 1,700 data entries to accomplish a 90% accuracy when predicting grades.'
+        ],
+        Award: 'Second Place, poster symposium at WSTS for our research.'
+    }
+]
