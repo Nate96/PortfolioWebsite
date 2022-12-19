5d70bcc3a8e0805cc7be60b4a2b1427985181799
Nate96 <braukhoff96@gmail.com>
2022-12-18 18:41:48 -0600

    name card

--- portfolio-website/src/App.js
+++ portfolio-website/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import React from 'react'
-import Experiences from './Components/Experiences.js';
+import Experiences from './Components/Experiences.js'
+import NameCard from './Components/NameCard';
 
 function App() {
   return (
     <div className="App">
+      <NameCard/>
       <h1>Experiences</h1>
       <Experiences/>
 
---
+++ portfolio-website/src/Components/NameCard.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Card from 'react-bootstrap/Card'
+import picture from './../data/Nate2.png'
+import 'bootstrap/dist/css/bootstrap.css';
+
+function NameCard() {
+    return (
+        <div style={{paddingBottom: '1rem'}}>
+            <Card className='nameCard' style={{
+                    maxWidth: '38rem', 
+                    maxHight: '10rem', 
+                    textAlign: 'left', 
+                }}>
+                <img className='card-img-top' src={picture} alt='Card image cap'/> 
+                <div className='card-body'>
+                    <h1 className='card-title'>Nate Braukhoff</h1>
+                    <p className='card-text' style={{fontSize: '18px'}}>(262)-794-6972</p>
+                    <p className='card-text' style={{fontSize: '18px'}}>nate.braukhoff@gmail.com</p>
+                </div>
+            </Card>
+        </div>
+
+    )
+}
+
+export default NameCard
